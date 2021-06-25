e75712fb7edd782b880bf93ca0f6656481821283
brothersoo <moistybro@gmail.com>
2021-06-25 16:03:36 +0900

    ADD: bean definition check test

---
+++ src/test/java/brothersoo/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,26 @@
+package brothersoo.core.beandefinition;
+
+import brothersoo.core.spring.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+  AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+  @Test
+  @DisplayName("Bean 설정 메타정보 확인")
+  void findApplicationBeanMetaData() {
+    String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+    for (String beanDefinitionName : beanDefinitionNames) {
+      BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+      if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+        System.out.printf("beanDefinitionName = %s   :   BeanDefinition = %s\n", beanDefinitionName,
+            beanDefinition);
+      }
+    }
+  }
+
+}
