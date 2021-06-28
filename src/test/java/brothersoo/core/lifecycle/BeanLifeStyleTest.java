56a0c33a2cad62e41073897b3607f44f5ebcb2d9
brothersoo <moistybro@gmail.com>
2021-06-28 13:24:58 +0900

    ADD: bean lifecycle test 1

---
+++ src/test/java/brothersoo/core/lifecycle/BeanLifeStyleTest.java
@@ -0,0 +1,33 @@
+package brothersoo.core.lifecycle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class BeanLifeStyleTest {
+
+  @Test
+  @DisplayName("Bean 생명주기 테스트")
+  void lifeStyleTest() {
+    ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
+        LifeStyleConfig.class);
+    NetworkClient networkClient = ac.getBean("networkClient", NetworkClient.class);
+    ac.close();
+  }
+
+  @Configuration
+  static class LifeStyleConfig {
+
+    @Bean
+    NetworkClient networkClient() {
+      NetworkClient networkClient = new NetworkClient();
+      networkClient.setUrl("brothersoo.github.io");
+      return networkClient;
+    }
+
+
+  }
+}
---
+++ src/test/java/brothersoo/core/lifecycle/NetworkClient.java
@@ -0,0 +1,31 @@
+package brothersoo.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient {
+
+  private String url;
+
+  public NetworkClient() {
+    System.out.printf("생성자 호출, url = %s\n", this.url);
+    connect();
+    call("초기화 연결 메세지");
+  }
+
+  void setUrl(String url) {
+    this.url = url;
+  }
+
+  void connect() {
+    System.out.printf("connect: %s\n", url);
+  }
+
+  void call(String message) {
+    System.out.printf("call: %s, message = %s\n", url, message);
+  }
+
+  void disconnect() {
+    System.out.printf("disconnect: %s\n", url);
+  }
+}
